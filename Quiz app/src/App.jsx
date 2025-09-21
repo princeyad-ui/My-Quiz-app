16a9821e453beeb93f322f148f6b2248b77d82cc
princeyad-ui <rajprincemth2018@gmail.com>
2025-09-21 17:24:09 +0530

    Added new feature

--- Quiz app/src/App.jsx
+++ Quiz app/src/App.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-import QuestionCard from "./pages/QuestionCard"
+import QuestionCard from "./pages/QuestionCard";
 import NavBar from "./pages/NavBar";
 import ReviewCard from './pages/ReviewCard';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route element={<NavBar/>} path="/navbar"/>
-      <Route element={<Home/>} path="/home"/>
-      <Route element={<Quiz/>} path="/quiz"/>
-      <Route element={<Result/>} path="/result"/>
-      <Route element={<QuestionCard/>} path="/questioncard"/>
-    
-     
-    </Routes>
+      <Routes>
+        {/* ✅ Redirect root URL to /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
+
+        {/* ✅ Your defined routes */}
+        <Route path="/navbar" element={<NavBar />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/questioncard" element={<QuestionCard />} />
+        <Route path="/reviewcard" element={<ReviewCard />} />
+      </Routes>
     </BrowserRouter>
   );
 };
