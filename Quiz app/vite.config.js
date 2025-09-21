664b592a902fe61407e226279452793d97e413b7
princeyad-ui <rajprincemth2018@gmail.com>
2025-09-21 17:30:19 +0530

    Update vite.config.js

--- Quiz app/vite.config.js
+++ Quiz app/vite.config.js
@@ -1,11 +1,11 @@
-// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: '/home', // 👈 Change this path to your desired start route
+    open: '/home', 
   },
 });
 
